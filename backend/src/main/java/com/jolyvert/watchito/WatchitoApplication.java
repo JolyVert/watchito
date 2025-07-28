d5a1a2a91a41db1a90d6f95351a05ccdbc1dc0cf
Anton <nous927@gmail.com>
2025-07-28 21:27:13 +0300

    first commit

---
+++ backend/src/main/java/com/jolyvert/watchito/WatchitoApplication.java
@@ -0,0 +1,13 @@
+package com.jolyvert.watchito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchitoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jolyvert/watchito/WatchitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolyvert.watchito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
